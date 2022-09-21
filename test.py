93edb333aebf6e47dd84e13619877a18f1c0710f
Punyapat <punyapat.pl@inetms.co.th>
2022-09-21 15:36:29 +0700

    edit test.py

--- test.py
+++ test.py
@@ -1,16 +1,37 @@
 import os
 import time
 import pexpect
+import os, signal
 
+
+count = 1
 server = '203.151.106.139'
 port = '10443'
 username = 'punyapat.pl'
 password = '1Net@Infra01'
 pid = os.getpid()
-# f = open('test_file.pid', 'r')
-# f = f.readline().strip()
+f = open('test_file.pid', 'w')
+f.write(int(pid))
+f.close
+
 
 cmd = "./forticlientsslvpn/64bit/forticlientsslvpn_cli --server " + server + ":" + port + " --vpnuser " + username
 
+print('Connect VPN')
 vpn = pexpect.spawn(cmd)
 print(vpn.expect_exact("Password for VPN:"))
+if 0 == vpn.expect_exact("Password for VPN:"): vpn.sendline(s=password)
+if 0 == vpn.expect_exact("Would you like to connect to this server? (Y/N)"): vpn.sendline(s="y")
+
+while 1:
+    if count < 6:
+        print(count)
+        time.sleep(1)
+        count += 1
+    else:
+        break
+
+f = f.readline().strip()
+
+print('Killing...')
+os.kill(int(f), signal.SIGTERM)
