1c0348a527f82412d2191ca78e140babe65e7949
Punyapat <punyapat.pl@inetms.co.th>
2022-09-21 15:16:55 +0700

    pexpect

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import os
 import time
+import pexpect
 
+server = '203.151.106.139'
+port = '10443'
+username = 'punyapat.pl'
+password = '1Net@Infra01'
 pid = os.getpid()
 # f = open('test_file.pid', 'r')
 # f = f.readline().strip()
-print(f)
+
+cmd = './forticlientsslvpn/64bit/forticlientsslvpn_cli --server serveraddress:port --vpnuser username'
+
+vpn = peexpect.spawn(cmd)
+print(vpn.expect_exact("Password for VPN:"))
